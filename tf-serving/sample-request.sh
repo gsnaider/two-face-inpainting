921df929f4b89ed4a500c26c0446c01ee597a475
Gaston Snaider <gnsnaider@gmail.com>
2018-11-30 15:46:10 -0300

    Scripts for running and calling tf serving

---
+++ tf-serving/sample-request.sh
@@ -0,0 +1,3 @@
+OUTPUT_FILE=patched_img
+
+curl -d "@sample_params" -X POST http://localhost:8501/v1/models/two_face_model:predict > $OUTPUT_FILE
---
+++ tf-serving/start-docker.sh
@@ -0,0 +1,3 @@
+MODEL_DIR=/home/gaston/workspace/two-face/two-face-inpainting-experiments/local-runs/save_model_test_4
+
+sudo docker run --runtime=nvidia -p 8501:8501   --mount type=bind,source=$MODEL_DIR,target=/models/two_face_model   -e MODEL_NAME=two_face_model -t tensorflow/serving:latest-gpu &
