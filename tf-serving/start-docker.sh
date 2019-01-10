4f950f4c6cbd925fcb441d7061c4461b783be40b
Gaston Snaider <gnsnaider@gmail.com>
2019-01-09 22:34:05 -0300

    Use trained model in tf-serving (still need to fix BN)

--- tf-serving/start-docker.sh
+++ tf-serving/start-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-MODEL_DIR=/home/gaston/workspace/two-face/two-face-inpainting-experiments/local-runs/save_model_test_4
+MODEL_DIR=/home/gaston/workspace/two-face/two-face-inpainting-experiments/trained-models/casia_vgg_rec_local_adv_global_adv_facenet_v4_5/saved_model
 
 sudo docker run --runtime=nvidia -p 8501:8501   --mount type=bind,source=$MODEL_DIR,target=/models/two_face_model   -e MODEL_NAME=two_face_model -t tensorflow/serving:latest-gpu &
