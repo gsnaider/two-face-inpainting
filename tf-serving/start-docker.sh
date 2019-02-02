ec7a508cb272285f5deaf8b977dc983d849e2a7f
Gaston Snaider <gnsnaider@gmail.com>
2019-02-01 23:00:18 -0300

    Update tf serving with latest model

--- tf-serving/start-docker.sh
+++ tf-serving/start-docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-MODEL_DIR=/home/gaston/workspace/two-face/two-face-inpainting-experiments/trained-models/two_face_v6_rec_loss/saved_model
+MODEL_DIR=/home/gaston/workspace/two-face/two-face-inpainting-experiments/trained-models/model_v6_1/two_face_v6_1_facenet/saved_model
 
+echo $MODEL_DIR
 sudo docker run --runtime=nvidia -p 8501:8501   --mount type=bind,source=$MODEL_DIR,target=/models/two_face_model   -e MODEL_NAME=two_face_model -t tensorflow/serving:latest-gpu &
