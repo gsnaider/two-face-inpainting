822c39c17e313d7514887e6922c704f55b6fb0aa
Gaston Snaider <gnsnaider@gmail.com>
2019-02-16 12:19:10 -0300

    Add license

--- src/trainer/generator.py
+++ src/trainer/generator.py
@@ -2,6 +2,37 @@ import tensorflow as tf
 
 IMAGE_SIZE = 128
 
+"""
+Channel-Wise Fully Connected Layer adapted from:
+https://github.com/jazzsaxmafia/Inpainting/blob/master/src/model.py
+
+Changed the original implementation from functional API to tf.keras.layers.Layer
+subclassing.
+
+Copyright (c) 2018, Taeksoo Kim
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
 class ChannelWiseFCLayer(tf.keras.layers.Layer):
   def __init__(self):
     super(ChannelWiseFCLayer, self).__init__()
